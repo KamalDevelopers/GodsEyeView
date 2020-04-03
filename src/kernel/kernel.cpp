1ba8bca9fcaffa80a6f30dd4a7cb19fa2a6fefd9
KamalDevelopers <leifminfagel@gmail.com>
2020-04-03 16:01:43 +0200

    Cleaned latest commit
    Removed accidental object files and cleaned the kernel main.

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -35,32 +35,6 @@ char* input(char brk, uint8_t color_index, KeyboardDriver *keyboard, Graphics *v
 extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
 {
 	Graphics vga;
-	
 	vga.Init(320, 200, 256, 0x0);
-	GlobalDescriptorTable gdt;
-	InterruptManager interrupts(0x20, &gdt);
-	GUI::Desktop desktop(320, 200, &vga);
-
-	MouseDriver mouse(&interrupts, &vga, &desktop);
-	interrupts.Activate();
-
-	GUI::Window window(44, 11, 200, 60, 0x1);
-	GUI::Window *win = &window;
-
-	GUI::Window windowTwo(80, 100, 160, 30, 0x3);
-	GUI::Window *winT = &windowTwo;
-
-	GUI::Label label(0, 50, 1, 10, 0x4, 0x2, "First Window");
-	GUI::Label *lbl = &label;
-
-	GUI::Label labelTwo(50, 0, 1, 10, 0x4, 0x2, "Second Window");
-	GUI::Label *lblT = &labelTwo;
-
-	win->AddWidget(1, lbl);
-	winT->AddWidget(1, lblT);
-
-	desktop.AddWin(2, win, winT);
-	desktop.Draw();
-
 	while(1);
 }
