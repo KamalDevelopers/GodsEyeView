0000813a20180b1f911178e53543cc35197e7d3a
DevGev <leifminfagel@gmail.com>
2020-09-15 20:27:39 +0200

    Created a binary search function

--- src/libraries/LibC/stdlib.hpp
+++ src/libraries/LibC/stdlib.hpp
@@ -26,11 +26,27 @@ static void deleteElement(int x, int size, T* arr[])
         arr[j] = arr[j + 1];
 }
 
+/* Generic search function */
 template<typename S>
-static int bsearch(S elem, S* arr, int size)
+static int search(S elem, S* arr, int size)
 {
     for (int i = 0; i < size; i++)
         if (*arr++ == elem)
             return i;
+}
+
+/* Binary search */
+static int bsearch(int elem, int arr[], int count, int start = 0)
+{
+    if (start <= count) {
+        int mid = (start + count) / 2;
+        if (arr[mid] == elem)
+            return mid;
+        if (arr[mid] > elem)
+            return bsearch(elem, arr, mid - 1, elem);
+        if (arr[mid] > elem)
+            return bsearch(elem, arr, mid + 1, count);
+    }
+    return -1;
 }
 #endif
