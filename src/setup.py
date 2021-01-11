daf9746d4cc080f9be3678fedb0f82aadf4dbe45
DevGev <leifminfagel@gmail.com>
2021-01-11 01:06:15 +0100

    Cleanup of build files

--- src/setup.py
+++ src/setup.py
@@ -4,11 +4,9 @@ import sys
 
 WRITEDISK = False 
 GCCPATH = '$HOME/opt/cross/bin/i686-elf-g++'
-GPPPARAMS = \
-    '-Ilibraries/LibC -Ilibraries-fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-leading-underscore -Wno-write-strings'
+GPPPARAMS = '-Ilibraries/LibC -Ilibraries-fno-use-cxa-atexit -fno-rtti -fno-exceptions -fno-leading-underscore -Wno-write-strings'
 ASPARAMS = '--32'
-QEMUPARAMS = \
-    '-cdrom kernel.iso -boot d -soundhw pcspk -serial mon:stdio -drive format=raw,file=../../hdd.tar'
+QEMUPARAMS = '-cdrom kernel.iso -boot d -soundhw pcspk -serial mon:stdio -drive format=raw,file=../../hdd.tar'
 
 filesC = [
     './kernel/kernel.cpp',
@@ -41,42 +39,22 @@ filesC = [
     './libraries/LibC/stdlib.cpp',
     './libraries/LibGUI/gui.cpp',
     ]
-
 filesA = ['./kernel/Hardware/interruptstubs.s']
-
-h = \
-    """
-setup.py make kernel - generate kernel object files
-setup.py make loader - generate loader object file
-setup.py link        - link files and generate kernel.bin
-setup.py clean       - remove all generated object files
-"""
-
 objectfiles = []
-
 
 def make_iso():
     os.system('mkdir out/iso')
     os.system('mkdir out/iso/boot')
     os.system('mkdir out/iso/boot/grub')
     os.system('cp out/kernel.bin out/iso/boot/kernel.bin')
-
     # os.system("echo 'set vbemode=1024x768x32'                      > out/iso/boot/grub/grub.cfg")
-
-    os.system("echo 'set timeout=0'                      > out/iso/boot/grub/grub.cfg"
-              )
-    os.system("echo 'set default=0'                     >> out/iso/boot/grub/grub.cfg"
-              )
-    os.system("echo ''                                  >> out/iso/boot/grub/grub.cfg"
-              )
-    os.system('echo \'menuentry "GevOS" {\' >> out/iso/boot/grub/grub.cfg'
-              )
-    os.system("echo '  multiboot /boot/kernel.bin'    >> out/iso/boot/grub/grub.cfg"
-              )
-    os.system("echo '  boot'                            >> out/iso/boot/grub/grub.cfg"
-              )
-    os.system("echo '}'                                 >> out/iso/boot/grub/grub.cfg"
-              )
+    os.system("echo 'set timeout=0'                      > out/iso/boot/grub/grub.cfg")
+    os.system("echo 'set default=0'                     >> out/iso/boot/grub/grub.cfg")
+    os.system("echo ''                                  >> out/iso/boot/grub/grub.cfg")
+    os.system('echo \'menuentry "GevOS" {\' >> out/iso/boot/grub/grub.cfg')
+    os.system("echo '  multiboot /boot/kernel.bin'    >> out/iso/boot/grub/grub.cfg")
+    os.system("echo '  boot'                            >> out/iso/boot/grub/grub.cfg")
+    os.system("echo '}'                                 >> out/iso/boot/grub/grub.cfg")
     os.chdir('./out')
     os.system('grub-mkrescue --output=kernel.iso iso')
     os.system('rm -rf iso')
@@ -87,12 +65,9 @@ def make_iso():
 
 def make_kernel():
     for file in filesC:
-        os.system(GCCPATH + ' ' + GPPPARAMS + ' -c ' + file + ' -o '
-                  + file[:-4] + '.o')
-
+        os.system(GCCPATH + ' ' + GPPPARAMS + ' -c ' + file + ' -o ' + file[:-4] + '.o')
     for file in filesA:
-        os.system('as ' + ASPARAMS + ' ' + file + ' -o' + file[:-2]
-                  + '.o')
+        os.system('as ' + ASPARAMS + ' ' + file + ' -o' + file[:-2] + '.o')
 
 def make_loader():
     os.system('as ' + ASPARAMS + ' loader.s -o loader.o')
@@ -105,16 +80,12 @@ def link():
 
     if not os.path.isdir('./out'):
         os.system('mkdir out > /dev/null 2>&1')
-
     os.system('rm out/*.iso > /dev/null 2>&1')
     os.system('rm out/*.bin > /dev/null 2>&1')
-    os.system('ld -melf_i386 -T linker.ld -o ./out/kernel.bin loader.o '
-               + ' '.join(objectfiles))
-
+    os.system('ld -melf_i386 -T linker.ld -o ./out/kernel.bin loader.o ' + ' '.join(objectfiles))
 
 def clean():
     os.system('rm *.o > /dev/null')
-
     ofolders = []
     for file in filesC:
         p = '/'.join(file.split('/')[0:-1])
@@ -131,7 +102,6 @@ def clean():
     if WRITEDISK == True:
         os.system('rm -rf tar.hdd')
 
-
 if len(sys.argv) < 2:
     print('Invalid amount of arguments\n')
     sys.exit()
@@ -139,19 +109,11 @@ if len(sys.argv) < 2:
 if sys.argv[1] == 'make':
     if sys.argv[2] == 'kernel':
         make_kernel()
-
     if sys.argv[2] == 'loader':
         make_loader()
-
     if sys.argv[2] == 'iso':
         make_iso()
-
 if sys.argv[1] == 'link':
     link()
-
 if sys.argv[1] == 'clean':
     clean()
-
-if sys.argv[1] == '--help' or sys.argv[1] == 'help' or sys.argv[1] \
-    == '-h':
-    print(h)
