08006b0ce2082fcaabe9fd693515830193e1c7f7
devgev <leifminfagel@gmail.com>
2022-01-19 16:48:09 +0100

    Prettier 'cat' implementation

--- Userland/Apps/Cat/main.cpp
+++ Userland/Apps/Cat/main.cpp
@@ -5,27 +5,28 @@
 
 int main(int argc, char** argv)
 {
-    if (argc != 0) {
-        int file_descriptor;
-        struct stat statbuffer;
-
-        file_descriptor = open((char*)argv[0]);
-        fstat(file_descriptor, &statbuffer);
-
-        if (statbuffer.st_size == -1) {
-            printf("File does not exist");
-            return 0;
-        }
-
-        char* buffer = (char*)malloc(sizeof(char) * statbuffer.st_size);
-
-        read(file_descriptor, buffer, statbuffer.st_size);
-        close(file_descriptor);
-
-        printf("%s", buffer);
-        free(buffer);
-    } else {
+    if (!argc) {
         printf("No input file");
+        return 0;
     }
+
+    int file_descriptor;
+    struct stat statbuffer;
+
+    file_descriptor = open((char*)argv[0]);
+    fstat(file_descriptor, &statbuffer);
+
+    if (statbuffer.st_size == -1) {
+        printf("File '%s' does not exist", (char*)argv[0]);
+        return 0;
+    }
+
+    char* buffer = (char*)malloc(sizeof(char) * statbuffer.st_size);
+
+    read(file_descriptor, buffer, statbuffer.st_size);
+    close(file_descriptor);
+
+    printf("%s", buffer);
+    free(buffer);
     return 0;
 }
