18e733fc816eeeee2788f45b36caa7cd0adcb0a5
devgev <leifminfagel@gmail.com>
2022-01-23 21:19:36 +0100

    Support more than one file argument in 'cat' program

--- Userland/Apps/Cat/main.cpp
+++ Userland/Apps/Cat/main.cpp
@@ -3,22 +3,17 @@
 #include <LibC/stdlib.hpp>
 #include <LibC/string.hpp>
 
-int main(int argc, char** argv)
+bool print_file(char* file_name)
 {
-    if (!argc) {
-        printf("No input file");
-        return 0;
-    }
 
     int file_descriptor;
     struct stat statbuffer;
 
-    file_descriptor = open((char*)argv[0]);
+    file_descriptor = open(file_name);
     fstat(file_descriptor, &statbuffer);
 
     if (statbuffer.st_size == -1) {
-        printf("File '%s' does not exist", (char*)argv[0]);
-        return 0;
+        return false;
     }
 
     char* buffer = (char*)malloc(sizeof(char) * statbuffer.st_size);
@@ -28,5 +23,23 @@ int main(int argc, char** argv)
 
     printf("%s", buffer);
     free(buffer);
+    return true;
+}
+
+int main(int argc, char** argv)
+{
+    if (!argc) {
+        printf("No input file");
+        return 0;
+    }
+
+    for (uint32_t i = 0; i < argc; i++) {
+        if (!print_file(argv[i])) {
+            printf("File '%s' does not exist", argv[i]);
+            if (i != argc - 1)
+                printf("\n");
+        }
+    }
+
     return 0;
 }
