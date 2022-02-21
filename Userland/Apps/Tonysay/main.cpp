d1c1ad406bcecbaf665db67f727a212813a7fdd8
devgev <leifminfagel@gmail.com>
2022-02-21 23:13:14 +0100

    Clear message buffer in 'tonysay' program

--- Userland/Apps/Tonysay/main.cpp
+++ Userland/Apps/Tonysay/main.cpp
@@ -44,6 +44,7 @@ int main(int argc, char** argv)
 {
     const char* default_msg = "I'm Tony the dog!";
     char msg[50];
+    memset(msg, 0, 50);
 
     if (!argc) {
         draw((char*)default_msg);
