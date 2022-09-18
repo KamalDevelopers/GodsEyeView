69b09589df41d2ed58a20719db4ee5d89385a63f
devgev <leifminfagel@gmail.com>
2022-09-18 13:07:02 +0200

    No longer send mouse position events to active window
    This process creates too much mouse lag. In the future we should think
    about sharing a buffer with the active window instead of using FIFOS for
    mouse events.

--- Userland/Servers/Display/window.cpp
+++ Userland/Servers/Display/window.cpp
@@ -81,10 +81,11 @@ void Window::mouse_event(mouse_event_t* event)
     if (!is_point_in_window(event->x, event->y))
         return;
 
-    display_event_t send_event;
-    send_event.type = DISPLAY_EVENT_MOUSE;
-    memcpy(&send_event.mouse, event, sizeof(mouse_event_t));
-    write(process_send_event_file, &send_event, sizeof(display_event_t));
+    /* Send mouse events to window */
+    /* display_event_t send_event; */
+    /* send_event.type = DISPLAY_EVENT_MOUSE; */
+    /* memcpy(&send_event.mouse, event, sizeof(mouse_event_t)); */
+    /* write(process_send_event_file, &send_event, sizeof(display_event_t)); */
 }
 
 void Window::keyboard_event(keyboard_event_t* event)
