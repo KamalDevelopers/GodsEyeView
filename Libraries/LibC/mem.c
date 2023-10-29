380ad56375d79cef659713b8cc61e80463402ef1
devgev <leifminfagel@gmail.com>
2023-10-29 13:15:42 +0100

    Implement memmove in LibC/mem.c

--- Libraries/LibC/mem.c
+++ Libraries/LibC/mem.c
@@ -14,6 +14,30 @@
 #define USE_CASE4
 #define USE_CASE5
 
+void* memmove(void* dest, const void* src, size_t n)
+{
+	uint8_t* from = (uint8_t*)src;
+	uint8_t* to = (uint8_t*)dest;
+
+	if (from == to || n == 0)
+		return dest;
+	if (to > from && to-from < (int)n) {
+		int i;
+		for(i=n-1; i>=0; i--)
+			to[i] = from[i];
+		return dest;
+	}
+	if (from > to && from-to < (int)n) {
+		size_t i;
+		for(i=0; i<n; i++)
+			to[i] = from[i];
+		return dest;
+	}
+	memcpy(dest, src, n);
+	return dest;
+}
+
+
 int memcmp(const void* buf1, const void* buf2, size_t count)
 {
     if (!count)
--- Libraries/LibC/mem.h
+++ Libraries/LibC/mem.h
@@ -9,6 +9,7 @@ extern void*(calloc)(size_t, size_t); ///< The standard function.
 extern void(free)(void*);             ///< The standard function.
 
 int memcmp(const void* buf1, const void* buf2, size_t count);
+void* memmove(void *dest, const void *src, size_t n);
 void* memchr(const void* str, int c, size_t n);
 void* memcpy32(void* dst, const void* src, size_t cnt);
 void* memcpy(void* dst, const void* src, unsigned int cnt);
