e77e76762433bb360893bb88c3a8b4e26fce1597
devgev <leifminfagel@gmail.com>
2023-11-05 20:23:10 +0100

    Add script align.py usage information

--- align.py
+++ align.py
@@ -4,9 +4,14 @@ import math
 import os
 import sys
 
+if len(sys.argv) < 3:
+    print("align.py [file] [alignment]")
+    print("padds 0's at end of file.")
+    exit()
+
 file_stats = os.stat(sys.argv[1])
 size = file_stats.st_size
-sects = math.floor(size / int(sys.argv[2])
+sects = math.floor(size / int(sys.argv[2]))
 overflow = size - sects * int(sys.argv[2])
 extra = int(sys.argv[2]) - overflow
 
