4d7d1bd23cd27b2dd521509cd028dc169a95b10c
devgev <leifminfagel@gmail.com>
2024-01-03 15:39:15 +0100

    Kernel eternal allocations for space efficiency

--- Kernel/Mem/mm.cpp
+++ Kernel/Mem/mm.cpp
@@ -2,33 +2,73 @@
 #include "../Hardware/interrupts.hpp"
 #include "paging.hpp"
 
+/* FIXME: Faults when < 0x1000 */
+#define KMALLOC_OFFSET 0x1000
+#define KETERNAL_MAX 1024
+
 typedef struct kmem_meta {
+    uint32_t magic;
     uint32_t address;
+    uint32_t pmm_address;
     uint32_t size;
-    uint8_t allocated;
 } kmem_meta;
 
-void* kmalloc(size_t size)
-{
-    kmem_meta meta;
-    uint32_t address = PMM->allocate_pages(PAGE_ALIGN(size) + PAGE_SIZE);
-    meta.size = size;
-    meta.address = address + PAGE_SIZE;
-    meta.allocated = 0x10;
-    memcpy((void*)address, (void*)&meta, sizeof(kmem_meta));
-    return (void*)meta.address;
-}
+typedef struct keternal {
+    uint32_t address;
+    uint32_t head;
+    uint32_t size;
+} keternal;
+
+static keternal keternal_page;
 
 void kfree(void* mem)
 {
-    kmem_meta meta;
-    memcpy(&meta, (void*)((uint32_t)mem - PAGE_SIZE), sizeof(kmem_meta));
-    if (meta.address != (uint32_t)mem || meta.allocated != 0x10) {
-        klog("invalid kfree() detected");
+    /* Eternal allocation */
+    if (((uint32_t)mem >= keternal_page.address) && ((uint32_t)mem <= (keternal_page.address + PAGE_SIZE)))
+        return;
+
+    /* Kernel heap allocation */
+    kmem_meta* meta = (kmem_meta*)((uint32_t)mem - KMALLOC_OFFSET);
+    if (meta->address != (uint32_t)mem || meta->magic != 0xBEEF) {
+        klog("invalid kfree() detected, magic=%x", meta->magic);
         return;
     }
-    PMM->free_pages((uint32_t)mem - PAGE_SIZE, meta.size + PAGE_SIZE);
-    meta.allocated = 0;
+
+    int total_size = meta->size + KMALLOC_OFFSET;
+    if ((total_size % PAGE_SIZE) != 0)
+        total_size = PAGE_ALIGN(total_size);
+
+    meta->magic = 0;
+    PMM->free_pages(meta->pmm_address, total_size);
+}
+
+void* kmalloc(size_t size)
+{
+    /* Eternal allocation */
+    if ((size <= KETERNAL_MAX) && ((keternal_page.size + size) < PAGE_SIZE)) {
+        if (!keternal_page.address) {
+            keternal_page.address = PMM->allocate_pages(PAGE_SIZE);
+            keternal_page.head = keternal_page.address;
+        }
+
+        uint32_t address = keternal_page.head;
+        keternal_page.head += size;
+        keternal_page.size += size;
+        return (void*)keternal_page.head;
+    }
+
+    /* Kernel heap allocation */
+    int total_size = size + KMALLOC_OFFSET;
+    if ((total_size % PAGE_SIZE) != 0)
+        total_size = PAGE_ALIGN(total_size);
+
+    uint32_t address = PMM->allocate_pages(total_size);
+    kmem_meta* meta = (kmem_meta*)address;
+    meta->size = size;
+    meta->address = address + KMALLOC_OFFSET;
+    meta->pmm_address = address;
+    meta->magic = 0xBEEF;
+    return (void*)meta->address;
 }
 
 void* operator new(size_t size)
